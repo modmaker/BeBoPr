84e5e3ea682bdb657487a7bfb9b0c60e86999069
Bas Laarhoven <sjml@xs4all.nl>
2012-06-01 08:46:06 +0200

    Made all output depending on DEBUG_TRAJECT setting.

--- traject.c
+++ traject.c
@@ -64,11 +64,11 @@ void traject_delta_on_all_axes( traject5D* traject)
   if (traject == NULL) {
     return;
   }
-  //  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    fprintf( stderr,  "\ntraject_delta_on_all_axes( traject(%lf,%lf,%lf,%lf,%u) ]\n",
-	     traject->dx, traject->dy, traject->dz, traject->de, traject->feed);
-    //  }
-  //  printf( "Acceleration constant c_acc = %1.6f\n", c_acc);
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "\ntraject_delta_on_all_axes( traject(%lf,%lf,%lf,%lf,%u) [m])\n",
+	    traject->dx, traject->dy, traject->dz, traject->de, traject->feed);
+    printf( "Acceleration constant c_acc = %1.6f\n", c_acc);
+  }
 
   double dx = traject->dx;
   double dy = traject->dy;
@@ -98,7 +98,9 @@ void traject_delta_on_all_axes( traject5D* traject)
   // We're only moving in 3D space, e-axis isn't part of this!
   double distance = sqrt( dx * dx + dy * dy + dz * dz);
   if (distance < 5.0E-9) {
-    printf( "*** Null move, distance = %1.9lf\n", distance);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "*** Null move, distance = %1.9lf\n", distance);
+    }
     return;	// TODO: will this suffice ?
   }
   /*
@@ -106,37 +108,51 @@ void traject_delta_on_all_axes( traject5D* traject)
    *  Determine feed per axis and clip to limits.
    */
   double recipr_dt = traject->feed / ( 60000.0 * distance);	/* [m/s] / [m] */
-  printf( "Request: total distance = %1.6lf [mm], speed = %1.3lf [mm/s] => time = %1.3lf [s]\n",
-	  SI2MM( distance), SI2MS( traject->feed / 60000.0), RECIPR( recipr_dt));
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Request: total distance = %1.6lf [mm], speed = %1.3lf [mm/s] => time = %1.3lf [s]\n",
+	    SI2MM( distance), SI2MS( traject->feed / 60000.0), RECIPR( recipr_dt));
+  }
   double vx = dx * recipr_dt;
   if (vx > vx_max) {	  // clip feed !
     recipr_dt = vx_max / dx;
-    printf( "*** clipping vx (%1.6lf) to vx_max (%1.6lf)\n", vx, vx_max);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "*** clipping vx (%1.6lf) to vx_max (%1.6lf)\n", vx, vx_max);
+    }
   }
   double vy = dy * recipr_dt;
   if (vy > vy_max) {	  // clip feed !
     recipr_dt = vy_max / dy;
-    printf( "*** clipping vy (%1.6lf) to vy_max (%1.6lf)\n", vy, vy_max);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "*** clipping vy (%1.6lf) to vy_max (%1.6lf)\n", vy, vy_max);
+    }
   }
   double vz = dz * recipr_dt;
   if (vz > vz_max) {	  // clip feed !
     recipr_dt = vz_max / dz;
-    printf( "*** clipping vz (%1.6lf) to vz_max (%1.6lf)\n", vz, vz_max);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "*** clipping vz (%1.6lf) to vz_max (%1.6lf)\n", vz, vz_max);
+    }
   }
   double ve = de * recipr_dt;
   if (ve > ve_max) {	  // clip feed !
     recipr_dt = ve_max / de;
-    printf( "*** clipping ve (%1.6lf) to ve_max (%1.6lf)\n", ve, ve_max);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "*** clipping ve (%1.6lf) to ve_max (%1.6lf)\n", ve, ve_max);
+    }
   }
   //  double dt = RECIPR( recipr_dt);	/* [s] */	// nodig ???
-  printf( "Limited values: time = %1.3lf [ms] => speed = %1.3lf [mm/s]\n",
-	  SI2MS( RECIPR( recipr_dt)), SI2MM( distance * recipr_dt));
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Limited values: time = %1.3lf [ms] => speed = %1.3lf [mm/s]\n",
+	    SI2MS( RECIPR( recipr_dt)), SI2MM( distance * recipr_dt));
+  }
   vx = dx * recipr_dt;
   vy = dy * recipr_dt;
   vz = dz * recipr_dt;
   ve = de * recipr_dt;
-  printf( "Velocities - X: %1.3lf, Y: %1.3lf, Z %1.3lf, E: %1.3lf [mm/s]\n",
-	  SI2MM( vx), SI2MM( vy), SI2MM( vz), SI2MM( ve));
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Velocities - X: %1.3lf, Y: %1.3lf, Z %1.3lf, E: %1.3lf [mm/s]\n",
+	    SI2MM( vx), SI2MM( vy), SI2MM( vz), SI2MM( ve));
+  }
   /*
    * For each axis the targeted speed is known now.
    *
@@ -153,15 +169,19 @@ void traject_delta_on_all_axes( traject5D* traject)
    * determine the largest period and scale the acceleration for each axis. */
 
   double t_acc = fmax( fmax( tx_acc, ty_acc), fmax( tz_acc, te_acc));
-  printf( "Acceleration duration - X: %1.3lf, Y: %1.3lf, Z: %1.3lf, E: %1.3lf => MAX=%1.3lf [ms]\n",
-	  SI2MS( tx_acc), SI2MS( ty_acc), SI2MS( tz_acc), SI2MS( te_acc), SI2MS( t_acc));
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Acceleration duration - X: %1.3lf, Y: %1.3lf, Z: %1.3lf, E: %1.3lf => MAX=%1.3lf [ms]\n",
+	   SI2MS( tx_acc), SI2MS( ty_acc), SI2MS( tz_acc), SI2MS( te_acc), SI2MS( t_acc));
+  }
   double recipr_t_acc = 1.0 / t_acc;
 
   double ax = vx * recipr_t_acc;
   double ay = vy * recipr_t_acc;
   double az = vz * recipr_t_acc;
   double ae = ve * recipr_t_acc;
-  printf( "Acceleration factor - X: %1.3lf, Y: %1.3lf, Z: %1.3lf, E: %1.3lf [m/s^2]\n", ax, ay, az, ae);
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Acceleration factor - X: %1.3lf, Y: %1.3lf, Z: %1.3lf, E: %1.3lf [m/s^2]\n", ax, ay, az, ae);
+  }
 
   /*
    * s = v^2/2a or s = a.t^2/2
@@ -173,8 +193,10 @@ void traject_delta_on_all_axes( traject5D* traject)
   double double_sy = ay * t_square;
   double double_sz = az * t_square;
   double double_se = ae * t_square;
-  printf( "Distance to reach full speed: - X:%1.6lf Y:%1.6lf Z:%1.6lf E:%1.6lf [mm]\n",
-	  SI2MM( 0.5 * double_sx), SI2MM( 0.5 * double_sy), SI2MM( 0.5 * double_sz), SI2MM( 0.5 * double_se));
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Distance to reach full speed: - X:%1.6lf Y:%1.6lf Z:%1.6lf E:%1.6lf [mm]\n",
+	    SI2MM( 0.5 * double_sx), SI2MM( 0.5 * double_sy), SI2MM( 0.5 * double_sz), SI2MM( 0.5 * double_se));
+  }
   //
   //  double half_t_square = 0.5 * t_acc * t_acc;
   double ramp_dx, ramp_dy, ramp_dz, ramp_de;
@@ -183,7 +205,9 @@ void traject_delta_on_all_axes( traject5D* traject)
 
   // Split traject into ramp-up, dwell and ramp-down
   if ((double_sx > dx) || (double_sy > dy) || (double_sz > dz) || (double_se > de)) {
-    printf( "*** Move is too short to accelerate to full speed!\n");
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "*** Move is too short to accelerate to full speed!\n");
+    }
 
     ramp_dx = 0.5 * dx;
     ramp_dy = 0.5 * dy;
@@ -200,8 +224,10 @@ void traject_delta_on_all_axes( traject5D* traject)
     vz = sqrt( az * dz);
     ve = sqrt( ae * de);
 
-    printf( "Maximum velocities - X: %1.3lf, Y: %1.3lf, Z %1.3lf, E: %1.3lf [mm/s]\n",
-	    SI2MM( vx), SI2MM( vy), SI2MM( vz), SI2MM( ve));
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Maximum velocities - X: %1.3lf, Y: %1.3lf, Z %1.3lf, E: %1.3lf [mm/s]\n",
+	      SI2MM( vx), SI2MM( vy), SI2MM( vz), SI2MM( ve));
+    }
   } else {
 
     ramp_dx = 0.5 * double_sx;
@@ -251,8 +277,10 @@ void traject_delta_on_all_axes( traject5D* traject)
     ramp_de = -ramp_de;
     dwell_de = -dwell_de;
   }
-  printf( "Slopes - X: %1.6lf, Y: %1.6lf, Z: %1.6lf, E: %1.6lf [mm] (duration=%1.3lf [ms])\n",
-	  SI2MM( ramp_dx), SI2MM( ramp_dy), SI2MM( ramp_dz), SI2MM( ramp_de), SI2MS( RECIPR( recipr_t_acc)));
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Slopes - X: %1.6lf, Y: %1.6lf, Z: %1.6lf, E: %1.6lf [mm] (duration=%1.3lf [ms])\n",
+	    SI2MM( ramp_dx), SI2MM( ramp_dy), SI2MM( ramp_dz), SI2MM( ramp_de), SI2MS( RECIPR( recipr_t_acc)));
+  }
   // TODO: make conditional: only for used axes ???
   uint32_t c0x =  (uint32_t) (c_acc * sqrt( step_size_x / ax));
   uint32_t c0y =  (uint32_t) (c_acc * sqrt( step_size_y / ay));
@@ -266,26 +294,34 @@ void traject_delta_on_all_axes( traject5D* traject)
   //  printf( "ramping up in %1.3lf [ms]\n", 1000 / dt);
   int any_move = 0;
   if (vx != 0.0) {
-    printf( "Queue X: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%u,cmin=%u)\n",
-	    SI2MM( vx), ax, SI2MM( ramp_dx), c0x, cminx);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue X: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%u,cmin=%u)\n",
+	      SI2MM( vx), ax, SI2MM( ramp_dx), c0x, cminx);
+    }
     pruss_queue_accel( 1, c0x, cminx, (int32_t)(1.0E9 * ramp_dx));
     any_move = 1;
   }
   if (vy != 0.0) {
-    printf( "Queue Y: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%d,cmin=%u)\n",
-	    SI2MM( vy), ay, SI2MM( ramp_dy), c0y, cminy);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue Y: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%d,cmin=%u)\n",
+	      SI2MM( vy), ay, SI2MM( ramp_dy), c0y, cminy);
+    }
     pruss_queue_accel( 2, c0y, cminy, (int32_t)(1.0E9 * ramp_dy));
     any_move = 1;
   }
   if (vz != 0.0) {
-    printf( "Queue Z: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%d,cmin=%u)\n",
-	    SI2MM( vz), az, SI2MM( ramp_dz), c0z, cminz);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue Z: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%d,cmin=%u)\n",
+	      SI2MM( vz), az, SI2MM( ramp_dz), c0z, cminz);
+    }
     pruss_queue_accel( 3, c0z, cminz, (int32_t)(1.0E9 * ramp_dz));
     any_move = 1;
   }
   if (ve != 0.0) {
-    printf( "Queue E: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%d,cmin=%u)\n",
-	    SI2MM( ve), ae, SI2MM( ramp_de), c0e, cmine);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue E: accelerate to %1.3lf [mm/s] with a=%1.3lf [m/s^2] over %1.3lf [mm] (c0=%d,cmin=%u)\n",
+	      SI2MM( ve), ae, SI2MM( ramp_de), c0e, cmine);
+    }
     pruss_queue_accel( 4, c0e, cmine, (int32_t)(1.0E9 * ramp_de));
     any_move = 1;
   }
@@ -295,26 +331,34 @@ void traject_delta_on_all_axes( traject5D* traject)
   }
   //
   if (dwell_dx != 0.0) {
-    printf( "Queue X: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
-	    SI2MM( vx), SI2MM( dwell_dx), cminx);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue X: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
+	      SI2MM( vx), SI2MM( dwell_dx), cminx);
+    }
     pruss_queue_dwell( 1, cminx, (int32_t)(1.0E9 * dwell_dx));
     any_move = 1;
   }
   if (dwell_dy != 0.0) {
-    printf( "Queue Y: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
-	    SI2MM( vy), SI2MM( dwell_dy), cminy);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue Y: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
+	      SI2MM( vy), SI2MM( dwell_dy), cminy);
+    }
     pruss_queue_dwell( 2, cminy, (int32_t)(1.0E9 * dwell_dy));
     any_move = 1;
   }
   if (dwell_dz != 0.0) {
-    printf( "Queue Z: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
-	    SI2MM( vz), SI2MM( dwell_dz), cminz);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue Z: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
+	      SI2MM( vz), SI2MM( dwell_dz), cminz);
+    }
     pruss_queue_dwell( 3, cminz, (int32_t)(1.0E9 * dwell_dz));
     any_move = 1;
   }
   if (dwell_de != 0.0) {
-    printf( "Queue E: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
-	    SI2MM( ve), SI2MM( dwell_de), cmine);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue E: dwell at speed %1.3lf [mm/s] over %1.6f [mm] (cmin=%u)\n",
+	      SI2MM( ve), SI2MM( dwell_de), cmine);
+    }
     pruss_queue_dwell( 4, cmine, (int32_t)(1.0E9 * dwell_de));
     any_move = 1;
   }
@@ -324,26 +368,34 @@ void traject_delta_on_all_axes( traject5D* traject)
   }
   //
   if (vx != 0.0) {
-    printf( "Queue X: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
-	    SI2MM( ramp_dx), ax);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue X: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
+	      SI2MM( ramp_dx), ax);
+    }
     pruss_queue_decel( 1, (int32_t)(1.0E9 * ramp_dx));
     any_move = 1;
   }
   if (vy != 0.0) {
-    printf( "Queue Y: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
-	    SI2MM( ramp_dy), ay);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue Y: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
+	      SI2MM( ramp_dy), ay);
+    }
     pruss_queue_decel( 2, (int32_t)(1.0E9 * ramp_dy));
     any_move = 1;
   }
   if (vz != 0.0) {
-    printf( "Queue Z: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
-	    SI2MM( ramp_dz), az);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue Z: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
+	      SI2MM( ramp_dz), az);
+    }
     pruss_queue_decel( 3, (int32_t)(1.0E9 * ramp_dz));
     any_move = 1;
   }
   if (ve != 0.0) {
-    printf( "Queue E: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
-	    SI2MM( ramp_de), ae);
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Queue E: decelerate over %1.6lf [mm] with a=-%1.3lf [m/s^2]\n",
+	      SI2MM( ramp_de), ae);
+    }
     pruss_queue_decel( 4, (int32_t)(1.0E9 * ramp_de));
     any_move = 1;
   }
@@ -359,8 +411,10 @@ static void pruss_axis_config( int axis, double step_size, int reverse)
   uint16_t ssn = 1024;
   uint16_t sst = (int) ssn * (step_size * 1.0E9 - ssi);
 
-  printf( "Set axis nr %d step size (%1.6lf) to %u + %u / %u [nm] and %s direction\n",
-	  axis, step_size, ssi, sst, ssn, (reverse) ? "reversed" : "normal");
+  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+    printf( "Set axis nr %d step size (%1.6lf) to %u + %u / %u [nm] and %s direction\n",
+	    axis, step_size, ssi, sst, ssn, (reverse) ? "reversed" : "normal");
+  }
   pruss_queue_config_axis( axis, ssi, sst, ssn, reverse);
 }
 
