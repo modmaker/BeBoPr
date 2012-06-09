ee96188bf5691be0f324b0f26486940e78971d38
Bas Laarhoven <sjml@xs4all.nl>
2012-06-09 15:04:19 +0200

    Fixed comment style so that it can also be used by the assembler.

--- algo2cmds.h
+++ algo2cmds.h
@@ -1,12 +1,11 @@
 
-/*
- *  PRUSS_MAGIC is in the reserved opcodes list, so there is
- *  less chance of finding it by accident. It identifies valid
- *  microcode for the BeagleBone. It should be located immediately
- *  after the first instruction (that should be jump to code start).
- *  UCODE_MAGIC identifies the application, in this case code for
- *  the BeBoPr (2191) board.
- */
+//  PRUSS_MAGIC is in the reserved opcodes list, so there is
+//  less chance of finding it by accident. It identifies valid
+//  microcode for the BeagleBone. It should be located immediately
+//  after the first instruction (that should be jump to code start).
+//  UCODE_MAGIC identifies the application, in this case code for
+//  the BeBoPr (2191) board.
+
 #define PRUSS_MAGIC             0xbeb0c0de
 #define UCODE_MAGIC             0xba512191
 
