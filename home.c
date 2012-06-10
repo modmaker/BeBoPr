9bd4b9fde19e58d8bc9a725ec6c0698c334452f8
Bas Laarhoven <sjml@xs4all.nl>
2012-06-10 21:48:48 +0200

    Fixed homing code for firmware v3. Acceleration is not yet implemented.

--- home.c
+++ home.c
@@ -16,6 +16,8 @@
 #include "gcode_process.h"
 #include "traject.h"
 #include "bebopr.h"
+#include "debug.h"
+
 
 static const double fclk = TIMER_CLOCK;
 
@@ -37,119 +39,69 @@ static int limsw_axis( axis_e axis, int reverse)
 static void run_home_one_axis( axis_e axis, int reverse, uint32_t feed)
 {
   int direction = (reverse) ? -1 : 1;
-  int steps = 0;
+  int steps;
   const char axisNames[] = { '?', 'X', 'Y', 'Z' };
-  // FIXME: remove code duplication from gcode_process.c !!!
-  double step_size;
+
   int pruss_axis = 0;
-  step_size = config_get_step_size( axis);
+  double step_size = config_get_step_size( axis);
   switch (axis) {
-  case x_axis:
-    pruss_axis = 1;
-    break;
-  case y_axis:
-    pruss_axis = 2;
-    break;
-  case z_axis:
-    pruss_axis = 3;
-    break;
+  case x_axis: pruss_axis = 1; break;
+  case y_axis: pruss_axis = 2; break;
+  case z_axis: pruss_axis = 3; break;
   default:
-    printf( "BUG: trying to home illegal axis (%d)!\n", axis);
+    fprintf( stderr, "BUG: trying to home illegal axis (%d)!\n", axis);
     pruss_axis = 0;
   }
-  double ds = step_size;	/* delta per interation */
-  const double c_acc = 191201673.632;	// 0.676 * fclk * sqrt( 2.0)
-
   if (feed > 3000) {
     feed = 3000;
   }
-
-  double dv = (double)feed / (60 * 1000);	/* [mm/min] -> [m/s] */
-  double a = 1.5;		/* [m/s^s] */
-  double a_max = (dv * dv) / (2.0 * ds);
-  if (a > a_max) {
-    a = a_max;
-  }
-  uint32_t c0;
-  uint32_t cmin = fclk * step_size / dv ;
-  double ramp_length = (dv * dv) / (2.0 * a);
-
-  c0 = (uint32_t) (c_acc * sqrt( step_size / a));
-  ds = step_size;
-  if (direction < 0) {
-    ds = -ds;
-    dv = -dv;
-    ramp_length = -ramp_length;
-  }
+  double speed = feed / 60000.0;
+  uint32_t cmin = fclk * step_size / speed ;
+  uint32_t c0 = cmin;
   pruss_queue_set_origin( pruss_axis);
+  /*
+   * Run towards the switch
+   */
   pruss_queue_set_accel( pruss_axis, c0);
-  printf( "  %c: ramp to speed %1.3lf [mm/s] in steps of %1.6lf [mm] with a=%1.3lf [m/s^2] over s=%1.6lf [mm], c0=%d, cmin=%d\n",
-	  axisNames[ pruss_axis], 1000 * dv, 1000 * ds, a, 1000 * ramp_length, c0, cmin);
-  int ramp_up = 1;
   steps = 0;
   while (limsw_axis( axis, reverse) == 0) {
-    if (ramp_up) {
-      pruss_queue_accel_more( pruss_axis, cmin, (int32_t)(1.0E9 * ds));
-      ramp_length -= ds;
-      if ((direction < 0 && ramp_length >= 0) || (direction >= 0 && ramp_length < 0)) {
-	ramp_up = 0;
-	printf( "  %c: dwelling to limitswitch in steps of %1.6lf [mm] at speed %1.3lf [mm/s], cmin=%d\n",
-		axisNames[ pruss_axis], 1000 * ds, 1000 * dv, cmin);
-      }
-    } else {
-      pruss_queue_dwell( pruss_axis, cmin, (int32_t)(1.0E9 * ds));
-    }
+    pruss_queue_dwell( pruss_axis, cmin, (int32_t)(direction * 1.0E9 * step_size));
     pruss_queue_execute();
-    ++steps;
     sched_yield();
+    ++steps;
   }
-#if 0
-  // TODO: calculate deceleration ???
-  printf( "  %c: limit switch reached in %d iterations, ramp down from speed %1.3lf [m/s] with a=-%1.3lf [m/s^2] over s=%1.6lf [m]\n",
- 	  axisNames[ pruss_axis], steps, vx, ax, dx);
-  pruss_queue_decel( pruss_axis, (int32_t)(1.0E9 * dx));
-  pruss_queue_execute();
-#else
-  printf( "  %c: limit switch reached in %d iterations\n", axisNames[ pruss_axis], steps);
-#endif
-  printf( "  %c: pausing for switch debounce\n", axisNames[ pruss_axis]);
+  /*
+   * Debounce and reverse direction
+   */
+  if (debug_flags & DEBUG_HOME) {
+    printf( "  %c: limit switch reached in %d iterations\n",
+	    axisNames[ pruss_axis], steps);
+    printf( "  %c: pausing for switch debounce\n", axisNames[ pruss_axis]);
+  }
   sleep( 1);
-  pruss_dump_position( pruss_axis);
-  // Reverse direction
+  if (debug_flags & DEBUG_HOME) {
+    pruss_dump_position( pruss_axis);
+  }
   direction = -direction;
-  dv = -0.1 * dv;
+  speed = -0.1 * speed;
   cmin = 10 * cmin;
-  ds = (direction < 0) ? -step_size : step_size;
-  a_max = (dv * dv) / fabs(2.0 * ds);
-  if (a > a_max) {
-    a = a_max;
-  }
-  c0 = (uint32_t) (c_acc * sqrt( step_size / a));
-  ramp_length = (direction < 0) ? -(dv * dv) / (2.0 * a) : (dv * dv) / (2.0 * a);
-  pruss_queue_set_accel( pruss_axis, c0);	// FIXME: uses old accel constant !
+  c0 = cmin;
+  /*
+   * Run away from the switch
+   */
+  pruss_queue_set_accel( pruss_axis, c0);
   steps = 0;
-  ramp_up = 1;
-  printf( "  %c: ramp to speed %1.3lf [mm/s] in steps of %1.6lf [mm] with a=%1.3lf [m/s^2] over s=%1.6lf [mm], c0=%d\n",
-	  axisNames[ pruss_axis], 1000 * dv, 1000 * ds, a, 1000 * ramp_length, c0);
-  while (limsw_axis( axis, (direction < 0) ? 0 : 1)) {
-    if (ramp_up) {
-      pruss_queue_accel_more( pruss_axis, cmin, (int32_t)(1.0E9 * ds));
-      ramp_length -= ds;
-      if ((direction < 0 && ramp_length >= 0) || (direction >= 0 && ramp_length < 0)) {
-	ramp_up = 0;
-	printf( "  %c: dwelling from limitswitch in steps of %1.6lf [mm] at speed %1.3lf [mm/s]\n",
-		axisNames[ pruss_axis], 1000 * ds, 1000 * dv);
-      }
-    } else {
-      pruss_queue_dwell( pruss_axis, cmin, (int32_t)(1.0E9 * ds));
-    }
+  while (limsw_axis( axis, reverse) != 0) {
+    pruss_queue_dwell( pruss_axis, cmin, (int32_t)(direction * 1.0E9 * step_size));
     pruss_queue_execute();
     sched_yield();
     ++steps;
   }
-  printf( "  %c: limit switch was released after %d iterations\n", axisNames[ pruss_axis], steps);
-  sleep( 1);
-  pruss_dump_position( pruss_axis);
+  if (debug_flags & DEBUG_HOME) {
+    printf( "  %c: limit switch was released after %d iterations\n",
+	    axisNames[ pruss_axis], steps);
+    pruss_dump_position( pruss_axis);
+  }
   pruss_queue_set_origin( pruss_axis);
 }
 
