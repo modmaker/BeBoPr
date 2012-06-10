8b7af3e74e83ac25c1100a337c9ef1ad5065cc00
Bas Laarhoven <sjml@xs4all.nl>
2012-06-10 21:25:27 +0200

    Regrouped the debug codes for easier setting of groups from M111.

--- debug.h
+++ debug.h
@@ -5,32 +5,30 @@
 #include "config.h"
 
 #ifdef	DEBUG
-	#define		DEBUG_PID	1
-	#define		DEBUG_DDA	2
-	#define		DEBUG_POSITION	4
-	#define		DEBUG_HOMING	8
-	#define		DEBUG_TRAJECT	16
-	#define		DEBUG_GCODE_PROCESS	32
-	#define		DEBUG_TEMP	0x0040
-	#define		DEBUG_ANALOG	0x0080
-	#define		DEBUG_HEATER	0x0100
-	#define		DEBUG_PWM	0x0200
-	#define		DEBUG_LIMSW	0x0400
-	#define		DEBUG_PRUSS	0x0800
+	#define		DEBUG_GCODE_PROCESS 	0x0001
+	#define		DEBUG_LIMSW		0x0002
+	#define		DEBUG_HOME		0x0004
+	#define		DEBUG_TRAJECT		0x0008
+	#define		DEBUG_PRUSS		0x0010
+	#define		DEBUG_POSITION		0x0020
+	#define		DEBUG_HEATER		0x0040
+	#define		DEBUG_PID		0x0080
+	#define		DEBUG_PWM		0x0100
+	#define		DEBUG_TEMP		0x0200
+	#define		DEBUG_ANALOG		0x0400
 #else
 	// by setting these to zero, the compiler should optimise the relevant code out
-	#define		DEBUG_PID	0
-	#define		DEBUG_DDA	0
-	#define		DEBUG_POSITION	0
-	#define		DEBUG_HOMING	0
-	#define		DEBUG_TRAJECT	0
 	#define		DEBUG_GCODE_PROCESS	0
-	#define		DEBUG_TEMP	0
-	#define		DEBUG_ANALOG	0
-	#define		DEBUG_HEATER	0
-	#define		DEBUG_PWM	0
-	#define		DEBUG_LIMSW	0
-	#define		DEBUG_PRUSS	0
+	#define		DEBUG_LIMSW		0
+	#define		DEBUG_HOME		0
+	#define		DEBUG_TRAJECT		0
+	#define		DEBUG_PRUSS		0
+	#define		DEBUG_POSITION		0
+	#define		DEBUG_HEATER		0
+	#define		DEBUG_PID		0
+	#define		DEBUG_PWM		0
+	#define		DEBUG_TEMP		0
+	#define		DEBUG_ANALOG		0
 #endif
 
 #define			DEBUG_ECHO	(1<<31)
