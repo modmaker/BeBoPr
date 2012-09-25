f9bbd086f43152752997844cd1cb0734cfa878e8
Bas Laarhoven <sjml@xs4all.nl>
2012-09-25 16:28:27 +0200

    Complete overhaul: Removed code duplication. Changed lots of debug output and comments. Fixed calculation of small ramps and timing constants.

--- traject.c
+++ traject.c
@@ -44,15 +44,15 @@ static inline int queue_move( const char* axis_name, double ramp, double a, doub
     }
     int axis = (aname < 'X') ? aname - 'E' + 4 : aname - 'X' + 1;
     if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-      printf( "Queue %c: ramping to and from %1.3lf [mm/s] "
-	      "with a=%1.3lf [m/s^2] over %1.6lf [mm] (c0=%u,cmin=%u)\n",
-	      aname, SI2MM( v), a, SI2MM( ramp + dwell), c0, cmin);
-    }
-    if (c0 < cmin) {
-      if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-	      printf( "Queue %c: motor starts at dwell speed, no acceleration needed\n", aname);
+      if (c0 != cmin) {
+        printf( "Queue %c: ramping to and from %1.3lf [mm/s] "
+		"with a=%1.3lf [m/s^2] over %1.6lf [mm] (c0=%u,cmin=%u)\n",
+		aname, SI2MM( v), a, SI2MM( ramp + dwell + ramp), c0, cmin);
+      } else {
+        printf( "Queue %c: running at %1.3lf [mm/s] "
+		"over %1.6lf [mm] (c0=%u,cmin=%u)\n",
+		aname, SI2MM( v), SI2MM( ramp + dwell + ramp), c0, cmin);
       }
-      c0 = cmin;
     }
     pruss_queue_accel( axis, c0, cmin, (int32_t)(1.0E9 * (ramp + dwell)));
     return 1;
@@ -61,6 +61,103 @@ static inline int queue_move( const char* axis_name, double ramp, double a, doub
 }
 
 #define QUEUE_MOVE( axis) queue_move( #axis, ramp_d##axis, a##axis, v##axis, dwell_d##axis, c0##axis, cmin##axis)
+
+static inline void axis_calc( const char* axis_name, double step_size_, double d, double double_s, double* ramp_d, double a, double* v, double* dwell_d, uint32_t* c0, uint32_t* cmin, double* recipr_t_acc, double* recipr_t_move)
+{
+  if (d != 0.0) {
+    char aname = *axis_name;
+    if (islower( aname)) {
+      aname = toupper( aname);
+    }
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "%c move : ", aname);
+    }
+    if (double_s > d) {
+      /*
+       * Move length is too short to reach full speed.
+       * Recalculate new (lower) top speed and remove the dwell.
+       *
+       * Ramp length becomes half the move length.
+       */
+      if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+        printf( "(can't reach full speed) ");
+      }
+      *v = sqrt( a * d);
+      *ramp_d = 0.5 * d;
+      *dwell_d = 0.0;
+      *recipr_t_acc = *v / *ramp_d;
+    } else {
+     /*
+      * Move has ramp up, constant velocity and ramp down phases
+      */
+      *ramp_d = 0.5 * double_s;
+      *dwell_d = d - double_s;
+    }
+   /*
+    * Update the time it takes for the entire move to complete.
+    * (All axes will generate the same duration).
+    */
+    if (*recipr_t_move == 0.0) {
+      *recipr_t_move = *v / (4 * *ramp_d + *dwell_d);
+      if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+        printf( "(set move duration to %1.3lf [ms]) ",
+		SI2MS( RECIPR( *recipr_t_move)));
+      }
+    }
+    if (*ramp_d < step_size_) {
+     /*
+      * Replace a move with ramps that are too short to execute by
+      * a single constant (slightly lower) velocity move.
+      */
+      if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+        printf( "(removed ramps smaller than stepsize) ");
+      }
+      *ramp_d  = 0.0;
+      *dwell_d = d;
+      *v = *dwell_d * *recipr_t_move;
+      if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+        printf( "\n   no ramps, dwell= %3.6lf [mm], velocity= %3.3lf [mm/s], duration= %1.3lf [ms]\n",
+		SI2MM( *dwell_d), SI2MM( *v), SI2MS( RECIPR( *recipr_t_move)));
+      }
+      *cmin = fclk * step_size_ / *v ;
+      *c0   = *cmin;
+    } else {
+      if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+        printf( "\n   ramp= %3.6lf [mm], dwell= %3.6lf [mm], velocity= %3.3lf [mm/s], duration= %1.3lf [ms]\n",
+		SI2MM( *ramp_d), SI2MM( *dwell_d), SI2MM( *v), SI2MS( (4 * *ramp_d + *dwell_d) / *v));
+      }
+      *cmin = fclk * step_size_ / *v ;
+      *c0   = (uint32_t) (c_acc * sqrt( step_size_ / a));
+      if (*c0 < *cmin) {
+       /*
+	* Does this really happen?
+	*/
+        if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+		printf( "   *** motor can start at dwell speed, no acceleration needed (setting c0 to cmin)\n");
+        }
+        *ramp_d  = 0.0;
+        *dwell_d = d;
+        *v = *dwell_d * *recipr_t_move;
+	if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+	  printf( "   ramp: %3.6lf [mm], dwell: %3.6lf [mm], velocity: %3.3lf [mm/s], duration: %1.3lf [ms]\n",
+		  SI2MM( *ramp_d), SI2MM( *dwell_d), SI2MM( *v), SI2MS( (4 * *ramp_d + *dwell_d) / *v));
+	}
+	*cmin = fclk * step_size_ / *v ;
+	*c0   = *cmin;
+      }
+    }
+  } else {
+   /*
+    * NOP
+    */
+    *ramp_d  = 0.0;
+    *dwell_d = 0.0;
+    *cmin = 0;
+    *c0   = 0;
+  }
+}
+
+#define AXIS_CALC( axis) axis_calc( #axis, step_size_##axis, d##axis, double_s##axis, &ramp_d##axis, a##axis, &v##axis, &dwell_d##axis, &c0##axis, &cmin##axis, &recipr_t_acc, &recipr_t_move)
 
 /*
  * All dimensions are in SI units and relative
@@ -76,7 +173,7 @@ void traject_delta_on_all_axes( traject5D* traject)
     time( &t0);
   }
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "\nMOVE[ #%lu %ds] traject_delta_on_all_axes( traject( %lf, %lf, %lf, %lf, F=%u) [m])\n",
+    printf( "\nMOVE[ #%lu %ds] traject_delta_on_all_axes( traject( %0.9lf, %1.9lf, %1.9lf, %1.9lf, F=%u) [m])\n",
 	    serno, (int)time( NULL)-(int)t0,
 	    traject->dx, traject->dy, traject->dz, traject->de, traject->feed);
   }
@@ -118,196 +215,126 @@ void traject_delta_on_all_axes( traject5D* traject)
     // If E is only moving axis, set distance from E
     distance = de;
   }
-  /*
-   *  Vector length and requested feed are known now.
-   *  Determine feed per axis and clip to limits.
-   */
+ /*
+  * Travel distance and requested velocity are now known.
+  * Determine the velocities for the individual axes
+  * using the distances and total duration of the move.
+  * If a calculated velocity is higher than the maximum
+  * allowed, slow down the entire move.
+  */
   double recipr_dt = traject->feed / ( 60000.0 * distance);	/* [m/s] / [m] */
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "Request: total distance = %1.6lf [mm], speed = %1.3lf [mm/s] => time = %1.3lf [s]\n",
-	    SI2MM( distance), SI2MS( traject->feed / 60000.0), RECIPR( recipr_dt));
+    printf( "Request: total distance = %1.6lf [mm], vector velocity = %1.3lf [mm/s] => est. time = %1.3lf [ms]\n",
+	    SI2MM( distance), SI2MS( traject->feed / 60000.0), SI2MS( RECIPR( recipr_dt)));
   }
+  int v_change = 0;
   double vx = dx * recipr_dt;
   if (vx > vx_max) {	  // clip feed !
-    recipr_dt = vx_max / dx;
     if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
       printf( "*** clipping vx (%1.6lf) to vx_max (%1.6lf)\n", vx, vx_max);
     }
+    recipr_dt = vx_max / dx;
+    v_change = 1;
   }
   double vy = dy * recipr_dt;
   if (vy > vy_max) {	  // clip feed !
-    recipr_dt = vy_max / dy;
     if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
       printf( "*** clipping vy (%1.6lf) to vy_max (%1.6lf)\n", vy, vy_max);
     }
+    recipr_dt = vy_max / dy;
+    v_change = 1;
   }
   double vz = dz * recipr_dt;
   if (vz > vz_max) {	  // clip feed !
-    recipr_dt = vz_max / dz;
     if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
       printf( "*** clipping vz (%1.6lf) to vz_max (%1.6lf)\n", vz, vz_max);
     }
+    recipr_dt = vz_max / dz;
+    v_change = 1;
   }
   double ve = de * recipr_dt;
   if (ve > ve_max) {	  // clip feed !
-    recipr_dt = ve_max / de;
     if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
       printf( "*** clipping ve (%1.6lf) to ve_max (%1.6lf)\n", ve, ve_max);
     }
+    recipr_dt = ve_max / de;
+    v_change = 1;
   }
-  //  double dt = RECIPR( recipr_dt);	/* [s] */	// nodig ???
-  if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "Limited values: time = %1.3lf [ms] => speed = %1.3lf [mm/s]\n",
-	    SI2MS( RECIPR( recipr_dt)), SI2MM( distance * recipr_dt));
+ /*
+  * If one or more velocity were limited by its maximum,
+  * some of the other values may be incorrect. Recalculate all.
+  */
+  if (v_change) {
+    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
+      printf( "Velocity changed to %1.3lf [mm/s] and duration to %1.3lf [ms] due to this clipping\n",
+	      SI2MM( distance * recipr_dt), SI2MS( RECIPR( recipr_dt)));
+    }
+    vx = dx * recipr_dt;
+    vy = dy * recipr_dt;
+    vz = dz * recipr_dt;
+    ve = de * recipr_dt;
   }
-  vx = dx * recipr_dt;
-  vy = dy * recipr_dt;
-  vz = dz * recipr_dt;
-  ve = de * recipr_dt;
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
     printf( "Velocities - X: %1.3lf, Y: %1.3lf, Z %1.3lf, E: %1.3lf [mm/s]\n",
 	    SI2MM( vx), SI2MM( vy), SI2MM( vz), SI2MM( ve));
   }
-  /*
-   * For each axis the targeted speed is known now.
-   *
-   * Determine how long it takes the slowest axis to reach its target speed.
-   * Calculate the acceleration for each axis from this value.
-   */
-
-  /* determine per axis acceleration and clip to max value */
+ /*
+  * For a neat linear move, all ramps must start and end at the same moment
+  * and have constant (or synchronized) accelation.
+  * Now that the targeted velocity is now known for each axis, determine
+  * how long it takes for that axis to reach its target speed using maximum
+  * acceleration. The slowest axis then scales the acceleration used for all axes.
+  */
   double tx_acc = vx * recipr_a_max_x;
   double ty_acc = vy * recipr_a_max_y;
   double tz_acc = vz * recipr_a_max_z;
   double te_acc = ve * recipr_a_max_e;
-  /* For a true linear move, all acceleration periods must be equal and constant,
-   * determine the largest period and scale the acceleration for each axis. */
-
+ /*
+  * determine the largest period and scale the acceleration for all axes.
+  */
   double t_acc = fmax( fmax( tx_acc, ty_acc), fmax( tz_acc, te_acc));
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "Acceleration duration - X: %1.3lf, Y: %1.3lf, Z: %1.3lf, E: %1.3lf => MAX=%1.3lf [ms]\n",
+    printf( "Time needed to reach velocity: X= %1.3lf, Y= %1.3lf, Z= %1.3lf, E= %1.3lf => MAX= %1.3lf [ms]\n",
 	   SI2MS( tx_acc), SI2MS( ty_acc), SI2MS( tz_acc), SI2MS( te_acc), SI2MS( t_acc));
   }
   double recipr_t_acc = 1.0 / t_acc;
-
   double ax = vx * recipr_t_acc;
   double ay = vy * recipr_t_acc;
   double az = vz * recipr_t_acc;
   double ae = ve * recipr_t_acc;
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "Acceleration constant - X: %1.3lf, Y: %1.3lf, Z: %1.3lf, E: %1.3lf [m/s^2]\n", ax, ay, az, ae);
+    printf( "Synchronized acceleration constants: X= %1.3lf, Y= %1.3lf, Z= %1.3lf, E= %1.3lf [m/s^2]\n", 
+	    ax, ay, az, ae);
   }
-
-  /*
-   * s = v^2/2a or s = a.t^2/2
-   */
+ /*
+  *  Length of acceleration/deceleration traject:
+  *    s = v^2/2a or s = a.t^2/2
+  */
   double t_square  = t_acc * t_acc;
   double double_sx = ax * t_square;
   double double_sy = ay * t_square;
   double double_sz = az * t_square;
   double double_se = ae * t_square;
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "Distance to reach full speed: - X:%1.6lf Y:%1.6lf Z:%1.6lf E:%1.6lf [mm]\n",
+    printf( "Distance to reach full speed: X= %1.6lf Y= %1.6lf Z= %1.6lf E= %1.6lf [mm]\n",
 	    SI2MM( 0.5 * double_sx), SI2MM( 0.5 * double_sy), SI2MM( 0.5 * double_sz), SI2MM( 0.5 * double_se));
   }
   double ramp_dx, ramp_dy, ramp_dz, ramp_de;
   double dwell_dx, dwell_dy, dwell_dz, dwell_de;
 
-  // Split traject into ramp-up, dwell and ramp-down
-  if ((double_sx > dx) || (double_sy > dy) || (double_sz > dz) || (double_se > de)) {
-    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-      printf( "*** Move is too short to accelerate to full speed!\n");
-    }
-
-    ramp_dx = 0.5 * dx;
-    ramp_dy = 0.5 * dy;
-    ramp_dz = 0.5 * dz;
-    ramp_de = 0.5 * de;
-
-    dwell_dx = 0.0;
-    dwell_dy = 0.0;
-    dwell_dz = 0.0;
-    dwell_de = 0.0;
-
-    vx = sqrt( ax * dx);
-    vy = sqrt( ay * dy);
-    vz = sqrt( az * dz);
-    ve = sqrt( ae * de);
-
-    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-      printf( "Maximum velocities - X: %1.3lf, Y: %1.3lf, Z %1.3lf, E: %1.3lf [mm/s]\n",
-	      SI2MM( vx), SI2MM( vy), SI2MM( vz), SI2MM( ve));
-    }
-  } else {
-
-    if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-      printf( "Initial ramps - X: %1.6lf, Y: %1.6lf, Z: %1.6lf, E: %1.6lf [mm])\n",
-	      SI2MM( 0.5 * double_sx), SI2MM( 0.5 * double_sy),
-	      SI2MM( 0.5 * double_sz), SI2MM( 0.5 * double_se));
-    }
-
-    if (dx != 0.0) {
-      ramp_dx = 0.5 * double_sx;
-      if (ramp_dx < step_size_x) {
-        if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-          printf( "ramp for X is fraction (%1.6f) of step size, setting ramp to 0\n", ramp_dx / step_size_x);
-        }
-        ramp_dx  = 0.0;
-        dwell_dx = dx;
-      } else {
-        dwell_dx = dx - double_sx;
-      }
-    } else {
-      ramp_dx  = 0.0;
-      dwell_dx = 0.0;
-    }
-    if (dy != 0.0) {
-      ramp_dy = 0.5 * double_sy;
-      if (ramp_dy < step_size_y) {
-        if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-          printf( "ramp for Y is fraction (%1.6f) of step size, setting ramp to 0\n", ramp_dy / step_size_y);
-        }
-        ramp_dy  = 0.0;
-        dwell_dy = dy;
-      } else {
-        dwell_dy = dy - double_sy;
-      }
-    } else {
-      ramp_dy  = 0.0;
-      dwell_dy = 0.0;
-    }
-    if (dz != 0.0) {
-      ramp_dz = 0.5 * double_sz;
-      if (ramp_dz < step_size_z) {
-        if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-          printf( "ramp for Z is fraction (%1.6f) of step size, setting ramp to 0\n", ramp_dz / step_size_z);
-        }
-        ramp_dz  = 0.0;
-        dwell_dz = dz;
-      } else {
-        dwell_dz = dz - double_sz;
-      }
-    } else {
-      ramp_dz  = 0.0;
-      dwell_dz = 0.0;
-    }
-    if (de != 0.0) {
-      ramp_de = 0.5 * double_se;
-      if (ramp_de < step_size_e) {
-        if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-          printf( "ramp for E is fraction (%1.6f) of step size, setting ramp to 0\n", ramp_de / step_size_e);
-        }
-        ramp_de  = 0.0;
-        dwell_de = de;
-      } else {
-        dwell_de = de - double_se;
-      }
-    } else {
-      ramp_de  = 0.0;
-      dwell_de = 0.0;
-    }
-  }
-  // Put sign back into distances
+  uint32_t c0x, c0y, c0z, c0e;
+  uint32_t cminx, cminy, cminz, cmine;
+  double recipr_t_move = 0.0;	// means: not set
+ /*
+  * Calculate the timing for all axes
+  */
+  AXIS_CALC( x);
+  AXIS_CALC( y);
+  AXIS_CALC( z);
+  AXIS_CALC( e);
+ /*
+  * Put the sign back into the deltas
+  */
   if (reverse_x) {
     ramp_dx = -ramp_dx;
     dwell_dx = -dwell_dx;
@@ -325,18 +352,9 @@ void traject_delta_on_all_axes( traject5D* traject)
     dwell_de = -dwell_de;
   }
   if (DEBUG_TRAJECT && (debug_flags & DEBUG_TRAJECT)) {
-    printf( "Slopes - X: %1.6lf, Y: %1.6lf, Z: %1.6lf, E: %1.6lf [mm] (duration=%1.3lf [ms])\n",
+    printf( "Ramps: X= %1.6lf, Y= %1.6lf, Z= %1.6lf, E= %1.6lf [mm], ramp duration= %1.3lf [ms]\n",
 	    SI2MM( ramp_dx), SI2MM( ramp_dy), SI2MM( ramp_dz), SI2MM( ramp_de), SI2MS( RECIPR( recipr_t_acc)));
   }
-  // TODO: make conditional: only for used axes ???
-  uint32_t c0x =  (uint32_t) (c_acc * sqrt( step_size_x / ax));
-  uint32_t c0y =  (uint32_t) (c_acc * sqrt( step_size_y / ay));
-  uint32_t c0z =  (uint32_t) (c_acc * sqrt( step_size_z / az));
-  uint32_t c0e =  (uint32_t) (c_acc * sqrt( step_size_e / ae));
-  uint32_t cminx = fclk * step_size_x / vx ;
-  uint32_t cminy = fclk * step_size_y / vy ;
-  uint32_t cminz = fclk * step_size_z / vz ;
-  uint32_t cmine = fclk * step_size_e / ve ;
 
  /*
   * Up from version v3.0 of the stepper firmware, the stepper driver does acceleration
