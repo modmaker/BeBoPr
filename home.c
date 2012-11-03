6999e1db04f830c906d6467e1e38d96398bddc27
Bas Laarhoven <sjml@xs4all.nl>
2012-11-03 01:15:04 +0100

    Fixed homing code for new PRUSS code that uses separate ramp and dwell calls.

--- home.c
+++ home.c
@@ -24,7 +24,7 @@ static const char axisNames[] = { '?', 'X', 'Y', 'Z', 'E' };
 
 // new_state is 1 when running towards the switch, 0 when running away from the switch!
 static int step_until_switch_change( axis_e axis, int reverse, int new_state, int pruss_axis,
-					uint32_t c0, uint32_t cmin, int direction, int32_t* position)
+				uint32_t c0, uint32_t cmin, int direction, int32_t* position, int32_t ramp)
 {
   /* Clear internal position information */
   {
@@ -33,7 +33,7 @@ static int step_until_switch_change( axis_e axis, int reverse, int new_state, in
     uint8_t mask;
     uint8_t invert;
     int     gpiobit;
-    double  delta;
+    int32_t delta;
 
     switch (axis) {
     case x_axis: gpiobit = (reverse) ? XMIN_GPIO : XMAX_GPIO; break;
@@ -52,21 +52,26 @@ static int step_until_switch_change( axis_e axis, int reverse, int new_state, in
     if (DEBUG_HOME && (debug_flags & DEBUG_HOME)) {
       printf( "  Home: axis %d, starting at virtPos= %d\n", pruss_axis, virtPosI);
     }
-    delta = (new_state) ? 0.500 : 0.010;
-    pruss_queue_accel( pruss_axis, 0, c0, cmin, *position + direction * SI2POS( delta));
+    delta = SI2POS( (new_state) ? 0.500 : 0.010);
+    if (delta > ramp) {
+      delta -= ramp;
+    } else {
+      delta = 0;
+    }
+    /*
+     * Start a ramp followed by a dwell that will be terminated by a limitswitch state change.
+     */
+    pruss_queue_accel( pruss_axis, 0, c0, cmin, *position + direction * ramp);
     pruss_queue_exec_limited( mask, (new_state) ? invert : ~invert);
-
+    pruss_queue_dwell( pruss_axis, cmin, *position + direction * (ramp + delta));
+    pruss_queue_exec_limited( mask, (new_state) ? invert : ~invert);
     traject_wait_for_completion();
+
     pruss_get_positions( pruss_axis, &virtPosI_new, NULL);
     if (DEBUG_HOME && (debug_flags & DEBUG_HOME)) {
       printf( "  Home: axis %d, ended at virtPos= %d\n",
               pruss_axis, virtPosI_new);
     }
-   /*
-    *  For a move terminated by a limitswitch state change, part of the
-    *  internal position information is now wrong. Fix this now.
-    */
-    pruss_queue_set_position( pruss_axis, virtPosI_new);
     int32_t delta_pos = virtPosI_new - virtPosI;
     if (DEBUG_HOME && (debug_flags & DEBUG_HOME)) {
       printf( "  %c: limit switch %s detected after %1.6lf [mm]\n",
@@ -91,6 +96,7 @@ static int run_home_one_axis( axis_e axis, int reverse, int32_t* position, uint3
   uint32_t c0;
   uint32_t cmin;
   double si_speed;
+  uint32_t ramp;
 
   switch (axis) {
   case x_axis: pruss_axis = 1; break;
@@ -109,33 +115,38 @@ static int run_home_one_axis( axis_e axis, int reverse, int32_t* position, uint3
   /*
    * Calculate settings for acceleration at 25% of maximum
    */
+  double a = 0.25 * a_max;
   si_speed = feed / 60000.0;
   cmin = fclk * si_step_size / si_speed ;
-  c0   = (uint32_t) (c_acc * sqrt( si_step_size / (0.25 * a_max)));
+  c0   = (uint32_t) (c_acc * sqrt( si_step_size / a));
+  ramp = SI2POS( si_speed * si_speed / ( 2 * a));
   /*
    * If we can start running faster than the target speed, no acceleration is needed
    */
   if (c0 < cmin) {
     c0 = cmin;
+    ramp = 0;
   }
   /*
    * Run towards the switch, reversing direction
    */
   int32_t old_position = *position;
-  if (!step_until_switch_change( axis, reverse, 1, pruss_axis, c0, cmin, direction, position)) {
+  if (!step_until_switch_change( axis, reverse, 1, pruss_axis, c0, cmin, direction, position, ramp)) {
     return 0;
   }
   si_speed = config_get_home_release_feed( axis) / 60000.0;
-  c0   = (uint32_t) (c_acc * sqrt( si_step_size / (0.25 * a_max)));
+  c0   = (uint32_t) (c_acc * sqrt( si_step_size / a));
   cmin = fclk * si_step_size / si_speed ;
+  ramp = SI2POS( si_speed * si_speed / ( 2 * a));
   if (c0 < cmin) {
     c0 = cmin;
+    ramp = 0;
   }
   direction = -direction;
   /*
    * Run away from the switch
    */
-  if (!step_until_switch_change( axis, reverse, 0, pruss_axis, c0, cmin, direction, position)) {
+  if (!step_until_switch_change( axis, reverse, 0, pruss_axis, c0, cmin, direction, position, ramp)) {
     return 0;
   }
   int32_t new_position = *position;
