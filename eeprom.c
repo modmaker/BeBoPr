b66d0846ae36a5f8e14b61d7bb4547bae706f191
Bas Laarhoven <sjml@xs4all.nl>
2013-02-18 13:41:52 +0100

    Added code to handle PRUSS code storage in EEPROM.

--- eeprom.c
+++ eeprom.c
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stddef.h>
 
 #include "bebopr.h"
 #include "eeprom.h"
@@ -43,10 +44,16 @@ union bone_cape_io_config {
   };
 };
 
+struct pru_code_block {
+  uint32_t                   opcodes[ 2048];
+};
+
 struct eeprom {
   union bone_cape_io_config cape_io_config;
   uint8_t                   step_io_config;
-  // put microcode here ?
+  uint8_t                   reserved[ 11];
+  struct pru_code_block     pru0_code;
+  struct pru_code_block     pru1_code;
 };
 
 int eeprom_get_step_io_config( const char* eeprom_path)
@@ -114,6 +121,108 @@ done:
   return result;
 }
 
+unsigned int eeprom_get_pru_code_offset( int pru_nr)
+{
+  unsigned int offset = 0;
+  switch (pru_nr) {
+  case 0: offset = offsetof( struct eeprom, pru0_code); break;
+  case 1: offset = offsetof( struct eeprom, pru1_code); break;
+  }
+  return offset;
+}
+
+/*
+ *  Write the file specified by fname to a pru code block in EEPROM.
+ */
+int eeprom_write_pru_code( const char* eeprom_path, int pru_nr, const char* fname)
+{
+  unsigned int offset = eeprom_get_pru_code_offset( pru_nr);
+  int result;
+  int ee_fd = -1;
+  int fs_fd = -1;
+  // Open destination (EEPROM) file at specified offset for writing
+  ee_fd = open( eeprom_path, O_WRONLY);
+  if (ee_fd < 0) {
+    perror( "Failed to open EEPROM for writing");
+    result = -1;
+    goto done;
+  }
+  result = lseek( ee_fd, offset, SEEK_SET);
+  if (result < 0) {
+    perror( "Failed to lseek EEPROM");
+    result = -1;
+    goto done;
+  }
+  if (result != offset) {
+    fprintf( stderr, "lseek returned position %d\n", result);
+  }
+  // Open source file for reading
+  fs_fd = open( fname, O_RDONLY);
+  if (fs_fd < 0) {
+    perror( "Failed to open source for reading");
+    result = -1;
+    goto done;
+  }
+  //  Write contents of file to EEPROM
+  for (int i = 0 ; i < 2048 ; ++i) {
+    uint32_t opcode;
+    int count = read( fs_fd, &opcode, sizeof( opcode));
+    if (count != sizeof( opcode)) {
+      // No more data (or some other failure), clear EEPROM location
+      opcode = 0xFFFFFFFF;
+    }
+    count = write( ee_fd, &opcode, sizeof( opcode));
+    if (count != sizeof( opcode)) {
+      perror( "Failed to write to EEPROM");
+      fprintf( stderr, "Failed to write opcode[%d] to EEPROM\n", i);
+      result = -1;
+      goto done;
+    }
+  }
+  //  Verify EEPROM contents with file
+  result = lseek( fs_fd, 0, SEEK_SET);
+  close( ee_fd);
+  ee_fd = open( eeprom_path, O_RDONLY);
+  if (ee_fd < 0) {
+    perror( "Failed to open EEPROM for reading");
+    result = -1;
+    goto done;
+  }
+  result = lseek( ee_fd, offset, SEEK_SET);
+  for (int i = 0 ; i < 2048 ; ++i) {
+    uint32_t ee_opcode;
+    uint32_t fs_opcode;
+    int count = read( fs_fd, &fs_opcode, sizeof( fs_opcode));
+    if (count != sizeof( fs_opcode)) {
+      // No more data (or some other failure), clear EEPROM location
+      fs_opcode = 0xFFFFFFFF;
+    }
+    count = read( ee_fd, &ee_opcode, sizeof( ee_opcode));
+    if (count != sizeof( ee_opcode)) {
+      perror( "Failed to read from EEPROM");
+      fprintf( stderr, "Failed to read opcode[%d] from EEPROM\n", i);
+      result = -1;
+      goto done;
+    }
+    if (ee_opcode != fs_opcode) {
+      fprintf( stderr, "EEPROM verification failed at opcode[%d]: is %08x, should be %08x.\n",
+              i, ee_opcode, fs_opcode);
+      result = -1;
+      goto done;
+    }
+  }
+  // Success
+  result = 0;
+
+done:
+  if (fs_fd >= 0) {
+    close( fs_fd);
+  }
+  if (ee_fd >= 0) {
+    close( ee_fd);
+  }
+  return result;
+}
 
 #ifdef STANDALONE
 
--- eeprom.h
+++ eeprom.h
@@ -6,5 +6,7 @@
 
 extern int eeprom_get_step_io_config( const char* eeprom_path);
 extern int eeprom_set_step_io_config( const char* eeprom_path, uint8_t value);
+extern unsigned int eeprom_get_pru_code_offset( const int pru_nr);
+extern int eeprom_write_pru_code( const char* eeprom_path, int pru_nr, const char* fname);
 
 #endif
