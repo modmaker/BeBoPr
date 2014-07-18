d083e7030f074268a5de942be7759d7bb14356f0
Bas Laarhoven <sjml@xs4all.nl>
2014-07-18 11:49:14 +0200

    Fixed null pointer ref on error situations.

--- sys_paths.c
+++ sys_paths.c
@@ -16,12 +16,12 @@ const char* sys_path_finder( char* buffer, size_t max_size, const char* path)
 
   int result = glob( path, GLOB_ERR | GLOB_NOSORT, NULL, &globbuf);
   if (result == GLOB_NOMATCH) {
-    buffer = NULL;    		// error: none found
-    *buffer = '\0';
+    *buffer = '\0';   		// error: none found
+    buffer = NULL;
   } else {
     if (globbuf.gl_pathc != 1) {
-      buffer = NULL;      	// error: more than one found
-      *buffer = '\0';
+      *buffer = '\0';     	// error: more than one found
+      buffer = NULL;
     } else {
       strncpy( buffer, globbuf.gl_pathv[ 0], max_size);
       printf( "sys_path_finder( '%s') returns '%s'\n", path, buffer);
